9509a78cb71d7ea6b1b9cf972fd98901c0f0c1d6
godnudybee <boninnutepe@hotmail.com>
2021-06-24 00:16:23 +0000

    Update transactions_controller.rb

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,8 @@
 class TransactionsController < ApplicationController
     def detail_transactions
         carte_id = params["carte_id"]
-        debut = params.member? "debut" ? params["debut"] : nil
-        fin = params.member? "fin" ? params["fin"] : nil
+        debut = params.member?("debut") ? params["debut"] : nil
+        fin = params.member?("fin") ? params["fin"] : nil
 
         tout = Transaction.where("carte_id" => carte_id)
         if debut.nil?
